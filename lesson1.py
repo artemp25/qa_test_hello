b84059551317cc3cfcef29e8544ba082009e92df
Артем Первов <artem.p@ns.local>
2025-05-16 17:57:08 +0300

    Добавлен урок 1: переменные и print

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+print("Автотест запущен")
+
+name = "Артём"
+experience = 2
+is_qa = True
+
+print(name, "работает в QA уже", experience, "года.")
+
+print(type(name))
+print(type(experience))
+print(type(is_qa))
